fd554b158772593ac7bb64f093116dbd7bed393e
sandeep <sandeepsanju9182@gmail.com>
2026-09-25 12:28:08 +0530

    API-Gateway in Static Routing

---
+++ ecom-api-service/src/test/java/com/debugd/info/ecomapiservice/EcomApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debugd.info.ecomapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiServiceApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
